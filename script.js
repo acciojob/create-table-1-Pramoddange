5a39c3aa107b31594ec8b2004aaf4bede16bc397
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-03 20:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
+	let table=document.querySelector('#sampleTable')
+	  var newRow = table.insertRow(0);
+    
+    // Insert new cells into the new row
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    
+    // Set the content for the new cells
+    cell1.innerHTML = "New cell1";
+    cell2.innerHTML = "New cell2";
+
   
 }
